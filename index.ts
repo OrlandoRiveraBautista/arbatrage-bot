307db850c326f27ae304950c8908349e567b0ad0
OrlandoRiveraBautista <47257942+OrlandoRiveraBautista@users.noreply.github.com>
2022-03-07 21:31:16 -0600

    Tested EthersJS with UniSwap V3 using TypeScript

---
+++ index.ts
@@ -0,0 +1,107 @@
+import { ethers } from "ethers";
+import { Pool } from "@uniswap/v3-sdk";
+import { Token } from "@uniswap/sdk-core";
+import { abi as IUniswapV3PoolABI } from "@uniswap/v3-core/artifacts/contracts/interfaces/IUniswapV3Pool.sol/IUniswapV3Pool.json";
+
+/** Pool provider */
+const provider = new ethers.providers.JsonRpcProvider(process.env.RPC_URL);
+
+/** Eth/USDC pool address. Pool addresses are used for swapping token. */
+const poolAddress = "0x8ad599c3A0ff1De082011EFDDc58f1908eb6e6D8";
+
+/** Instance of pool, smart contract  */
+// new ethers.Contract( address , abi , signerOrProvider )
+const poolContract = new ethers.Contract(
+  poolAddress,
+  IUniswapV3PoolABI,
+  provider
+);
+
+/** Interface for pool values that dont change: Immutable */
+interface Immutables {
+  factory: string;
+  token0: string;
+  token1: string;
+  fee: number;
+  tickSpacing: number;
+  maxLiquidityPerTick: ethers.BigNumber;
+}
+
+/** Return Interface for the state of the Liquidity Pool */
+interface State {
+  liquidity: ethers.BigNumber;
+  sqrtPriceX96: ethers.BigNumber;
+  tick: number;
+  observationIndex: number;
+  observationCardinality: number;
+  observationCardinalityNext: number;
+  feeProtocol: number;
+  unlocked: boolean;
+}
+
+/** Function to get the pool immutable values */
+async function getPoolImmutables(): Promise<Immutables> {
+  const [factory, token0, token1, fee, tickSpacing, maxLiquidityPerTick] =
+    await Promise.all([
+      poolContract.factory(),
+      poolContract.token0(),
+      poolContract.token1(),
+      poolContract.fee(),
+      poolContract.tickSpacing(),
+      poolContract.maxLiquidityPerTick(),
+    ]);
+
+  const immutables: Immutables = {
+    factory,
+    token0,
+    token1,
+    fee,
+    tickSpacing,
+    maxLiquidityPerTick,
+  };
+  return immutables;
+}
+
+/** Function to return the pool state */
+async function getPoolState() {
+  const [liquidity, slot] = await Promise.all([
+    poolContract.liquidity(),
+    poolContract.slot0(),
+  ]);
+
+  const PoolState: State = {
+    liquidity,
+    sqrtPriceX96: slot[0],
+    tick: slot[1],
+    observationIndex: slot[2],
+    observationCardinality: slot[3],
+    observationCardinalityNext: slot[4],
+    feeProtocol: slot[5],
+    unlocked: slot[6],
+  };
+
+  return PoolState;
+}
+
+async function main() {
+  // get the pool's immutable and state values
+  const [immutables, state] = await Promise.all([
+    getPoolImmutables(),
+    getPoolState(),
+  ]);
+
+  const TokenA = new Token(3, immutables.token0, 1, "USDC", "USD Coin"); // creating Token instance of USDC from the pool immutables
+  const TokenB = new Token(3, immutables.token1, 18, "WETH", "Wrapped Ether"); // creating Token instance of ETH from the pool immutables
+
+  const poolExample = new Pool(
+    TokenA,
+    TokenB,
+    immutables.fee,
+    state.sqrtPriceX96.toString(),
+    state.liquidity.toString(),
+    state.tick
+  );
+  console.log(poolExample);
+}
+
+main();
